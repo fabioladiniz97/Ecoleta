483f4e393c8f56dc61908d90ebbf2738710a25ce
fabiola <fabioladiniz36@hotmail.com>
2020-06-03 21:50:59 -0300

    segunda aula

---
+++ scripts/create-point.js
@@ -0,0 +1,41 @@
+function populateUFs () {
+
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
+    .then( res => res.json()) /* função anonima que retorna valor*/
+    .then( states => {
+        for ( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+
+    } )
+}
+
+populateUFs()
+
+function getCities(event){
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+    const ufValue = event.target.value
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`
+    
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+    
+    fetch(url)
+    .then( res => res.json())
+    .then( cities => {
+        for ( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+        citySelect.disabled = false
+    } )
+}
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change",getCities)
